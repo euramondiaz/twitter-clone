9b255b671e0433aa247078cf73e0fe5591c1e146
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-16 14:23:38 -0300

    update footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { PostComponent } from './components/post/post.component';
 import { TweetComponent } from './components/tweet/tweet.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent,
-     PostComponent, TweetComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet,
+     TweetComponent, HomeComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PostComponent } from '../../components/post/post.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ PostComponent],
+  imports: [ HeaderComponent, FooterComponent, PostComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
